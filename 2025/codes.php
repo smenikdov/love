d7aca20c6bbe8b7ca5e89d921b74caf82aa55723
Stoic <sme.nik.dov@gmail.com>
2024-11-22 17:12:00 +0500

    -

--- 2025/codes.php
+++ 2025/codes.php
@@ -2,18 +2,18 @@
 header('Content-Type: application/json');
 
 $codes = [
-    ['code' => '1234', 'availableFrom' => new DateTime('2025-01-01', new DateTimeZone('Asia/Yekaterinburg'))],
-    ['code' => '1234', 'availableFrom' => new DateTime('2025-02-01', new DateTimeZone('Asia/Yekaterinburg'))],
-    ['code' => '1234', 'availableFrom' => new DateTime('2025-03-01', new DateTimeZone('Asia/Yekaterinburg'))],
-    ['code' => '1234', 'availableFrom' => new DateTime('2025-04-01', new DateTimeZone('Asia/Yekaterinburg'))],
-    ['code' => '1234', 'availableFrom' => new DateTime('2025-05-01', new DateTimeZone('Asia/Yekaterinburg'))],
-    ['code' => '1234', 'availableFrom' => new DateTime('2025-06-01', new DateTimeZone('Asia/Yekaterinburg'))],
-    ['code' => '1234', 'availableFrom' => new DateTime('2025-07-01', new DateTimeZone('Asia/Yekaterinburg'))],
-    ['code' => '1234', 'availableFrom' => new DateTime('2025-08-01', new DateTimeZone('Asia/Yekaterinburg'))],
-    ['code' => '1234', 'availableFrom' => new DateTime('2025-09-01', new DateTimeZone('Asia/Yekaterinburg'))],
-    ['code' => '1234', 'availableFrom' => new DateTime('2025-10-01', new DateTimeZone('Asia/Yekaterinburg'))],
-    ['code' => '1234', 'availableFrom' => new DateTime('2025-10-01', new DateTimeZone('Asia/Yekaterinburg'))],
-    ['code' => '1234', 'availableFrom' => new DateTime('2025-12-01', new DateTimeZone('Asia/Yekaterinburg'))],
+    ['code' => '8825', 'availableFrom' => new DateTime('2025-01-01', new DateTimeZone('Asia/Yekaterinburg'))],
+    ['code' => '1432', 'availableFrom' => new DateTime('2025-02-01', new DateTimeZone('Asia/Yekaterinburg'))],
+    ['code' => '1137', 'availableFrom' => new DateTime('2025-03-01', new DateTimeZone('Asia/Yekaterinburg'))],
+    ['code' => '5701', 'availableFrom' => new DateTime('2025-04-01', new DateTimeZone('Asia/Yekaterinburg'))],
+    ['code' => '4743', 'availableFrom' => new DateTime('2025-05-01', new DateTimeZone('Asia/Yekaterinburg'))],
+    ['code' => '9812', 'availableFrom' => new DateTime('2025-06-01', new DateTimeZone('Asia/Yekaterinburg'))],
+    ['code' => '7163', 'availableFrom' => new DateTime('2025-07-01', new DateTimeZone('Asia/Yekaterinburg'))],
+    ['code' => '7733', 'availableFrom' => new DateTime('2025-08-01', new DateTimeZone('Asia/Yekaterinburg'))],
+    ['code' => '2699', 'availableFrom' => new DateTime('2025-09-01', new DateTimeZone('Asia/Yekaterinburg'))],
+    ['code' => '4591', 'availableFrom' => new DateTime('2025-10-01', new DateTimeZone('Asia/Yekaterinburg'))],
+    ['code' => '7379', 'availableFrom' => new DateTime('2025-10-01', new DateTimeZone('Asia/Yekaterinburg'))],
+    ['code' => '0450', 'availableFrom' => new DateTime('2025-12-01', new DateTimeZone('Asia/Yekaterinburg'))],
 ];
 
 // возвращает номер месяца (1–12)
